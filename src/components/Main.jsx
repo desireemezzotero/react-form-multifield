746c39da84041be32fad483f5adce0bc80c818d3
desireemezzotero <desireemezzotero1@gmail.com>
2025-01-20 19:24:03 +0100

    card

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,43 +1,137 @@
 import React from 'react';
 import { useState } from 'react';
 
-const list = [
-  "fare la spesa",
-  "dare da mangiare al cane",
-  "lavare casa"
-]
+const categories = [
+  "Tecnologia",
+  "Moda",
+  "Cibo",
+  "Viaggi",
+  "Sport",
+  "Arte",
+  "Musica",
+  "Cinema",
+  "Educazione",
+  "Salute"
+];
+
+const tags = [
+    "Azione",
+    "Commedia",
+    "Dramma",
+    "Fantascienza",
+    "Horror",
+    "Fantascienza",
+  ];
+
 
 const Main = () => {
 
-  const [task, setTask] = useState(list)
-  const [newTask, setNewTask] = useState('')
+  const [form, setForm] = useState({
+    title: '',
+    description: '',
+    image: '',
+    category: '',
+    tag: [],
+    published: false
+  })
 
-  const handlerSumbit = (e) => {
-   e.preventDefault()
-   setTask([newTask, ...task])
-   if (newTask == '') {
-    alert('Inserisci cosa devi fare')
-   }
-   setNewTask('')
+  const newPost = (e) => {
+    let { title, description, image, category, tag, published} = e.target
+    setForm({
+      ...form,
+      [name] : value
+    })
   }
+
+ const handlerSumbit = (e) => {
+  e.preventDefault()
+ }
+
 
   return (
     <main>
-      <div className="container m-5 text-center">
-      <form className="form-inline d-flex" onSubmit={handlerSumbit}>
-       <input className="form-control mr-sm-2" 
-          type="text" 
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
-          placeholder="Aggiungi cosa devi fare" 
-          aria-label="Search" />
-       <button className="btn btn-outline-success my-2 my-sm-0 m-3" type="submit">Search</button>
+      <div className="container m-5">
+      <form className="form-inline" onSubmit={handlerSumbit}>
+
+        {/* titolo */}
+        <input className="form-control mr-sm-2 mb-3" 
+          type="text"
+          name="title"
+          value={form.title}
+          placeholder="Titolo" 
+          onChange={newPost}
+        />
+
+        {/* contenuto */}
+        <input className="form-control mr-sm-2 mb-3" 
+          type="text"
+          name="description"
+          value={form.description}
+          placeholder="Descrizione dell'articolo" 
+          onChange={newPost}
+        />
+
+       {/* immagine */}
+       <input className="form-control mr-sm-2 mb-3" 
+          type="file"
+          name="image"
+          value={form.image}
+          placeholder="url" 
+          onChange={newPost}
+        />
+
+       {/* categoria */}
+       <select className="form-select mb-3">
+        <option selected>Scegli la categoria dell'articolo</option>
+          {categories.map((e,index) => 
+            <option key={index} value={e}>{e}</option>
+          )}
+       </select>
+
+       {/* tags */}
+       <div className="form-check mb-3">
+        <h5>Seleziona il genere</h5>
+        {tags.map ((tag,index) => 
+        <>
+        <div key={index}>
+         <input 
+            className="form-check-input" 
+            type="checkbox" 
+            name="tags"
+            value={tag}
+            onChange={newPost}
+          />
+          <label className="form-check-label" key={`check-${index}`}>
+            {tag}
+          </label>
+        </div>
+         </>
+        )}
+       </div> 
+
+       {/* categoria */}
+       <select className="form-select mb-3" >
+        <option selected>Scegli se pubblicare o meno il tuo articolo</option>
+          <option value="true">Bozze</option>
+          <option value="false">Pubblica online</option>
+       </select>
       </form>
 
-     <ul className="list-group m-2">
-        {task.map((work, index) => (
-         <li key={index} className="list-group-item">{work}</li> ))}    
-     </ul> 
+      <button className="btn btn-outline-success my-2 my-sm-0 m-3" type="submit">INVIA</button>
+
+      <div className="card">
+      {form.title && (
+          <div className="card-body">
+            {form.image && <img src={form.image} alt="article" />}
+            <h5 className="card-title">{form.title}</h5>
+            <p>{form.description}</p>
+            <p><strong>Categoria:</strong> {form.category}</p>
+            <p><strong>Tags:</strong> {form.tag.join(", ")}</p>
+            <p><strong>Pubblicato:</strong> {form.published ? "Bozze" : "Online"}</p>
+          </div>
+        )}
+
+      </div>
    </div>
     </main>
   )
